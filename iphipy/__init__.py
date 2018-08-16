95405769a8b2a590a955a25c1226fd44ab3876d5
Unknown <volzstefan97@googlemail.com>
2018-08-16 21:36:30 +0200

    Changed Example/Test

--- iphipy/__init__.py
+++ iphipy/__init__.py
@@ -2,7 +2,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
-import timeit.time
 
 import Basics.Basics as base
 import Basics.Complex as cmplx
@@ -10,72 +9,49 @@ import Basics.Convert as conv
 import Basics.Filters as fltr
 import Basics.Systems as sys
 import Metrology
+import threading
 
-
+sp.init_printing()
 """Package Doc
 ToDo:
-WIP Multiprocess Evaluation of Systems to speed up the currently exorbitantly long calculation times
+Multiprocess Evaluation of Systems to speed up the currently exorbitantly long calculation times or implementing calculations in C
+Renaming all modules to snake case
 
+"""
+"""Uncomment on release
 if __name__ == "__main__":
     raise ImportWarning("This is a module and should not to be executed")
 """
-def function_():
+
+def system_test():
     print("")
-    print("---"*20)
+    print(" -- -"*20)
     print("Examples")
-    print("---"*20)
+    print(" -- -"*20)
     sp.init_printing()
-    try:
-        f = sys._ee_symbol("f")
-        C1 = sys.Capacitor("C1", 100e-6, f)
-        R1 = sys.Resistor("R1", 100)
-        Z1 = sys.System("Z1", (C1,R1))
-        print(R1.symbolic_impedance)
-        print(C1.symbolic_impedance)
-        print(Z1.symbolic_impedance)    
-        f = np.linspace(0,20e3,5e3)
-        frequency_band = f[1:]
-        Z1_nyquist = []
-        i = 0
-        print(Z1.admittance)
-        for f in frequency_band:
-            print(i)
-            i += 1
-            a = complex(Z1._admittance.subs(C1.frequency, f))
-            Z1_nyquist.append(a)
-        Z1_nyquist = np.asarray(Z1_nyquist)
-        plt.plot(Z1_nyquist.real, Z1_nyquist.imag)
-    except DeprecationWarning as err:
-        print(err.__str__)
-
-    print("END")
-
-def calculate():
-    for i in range(start, stop):
-        
-
-def multiprocess(function_, number_of_processes, total_range):
-    """
-    Args:
-        function_ (function): Function that is to be multiprocessed
-        numer_of_processes (int): Number of processes to spawn
-        total_range (iterable of numeric): min and max value to pass to function_
-    """
-    processes = [Process(target = calculate, args = (total_range/number_of_processes)) for i in range(number_of_processes)]
-    
-    step_size = (total_range[-1]-total_range[0])/number_of_processes
-    for i in range(number_of_processes):
-        
-        
-        processes.append(Process(target = calculate, args = ()))
-
-    for process in processes:
-        process.start()
-    
-    for process in processes:
-        process.join()
-t0 = time.time()
-function_()
-t1 = time.time()
-plt.show()
-print(t1-t0)
+    f = sys._ee_symbol("f") # define your frequency
+    R1 = sys.Resistor("R1", 100) # define your components
+    R2 = sys.Resistor("R2", 100)
+    L1 = sys.Inductance("L1", 10e-3, f)
+    C1 = sys.Capacitor("C1", 20e-6, f)
+    C2 = sys.Capacitor("C2", 10e-3, f)
+    Z1 = sys.System("Z1", (R1, L1, C1, R2)) # define your circuit
+    Z2 = sys.System("Z2", (C2, Z1), "parallel")  
+    p1 = Z2.nyquist(range(0, int(20e3)), f, mode = "impedance") # plot everything
+    p2 = Z2.nyquist(range(0, int(20e3)), f, mode = "admittance")
+    p1.join()
+    p2.join()
+    print("Symbolic expressions are:") # get all your equations
+    print("Z1 = {}".format(Z1.symbolic_impedance))
+    print("Z2 = {}".format(Z2.symbolic_impedance))
+    print("Y1 = {}".format(Z1.symbolic_admittance))
+    print("Y2 = {}".format(Z2.symbolic_admittance))
+    print("Partially evaluated expressions are:")
+    print("Z1 = {}".format(Z1.impedance))
+    print("Z2 = {}".format(Z2.impedance))
+    print("Y1 = {}".format(Z1.admittance))
+    print("Y2 = {}".format(Z2.admittance))
+    print("f_resonance = {}Hz".format(Z2.resonance(f))) # do some fancy maths and get your resonance frequency ( or not if your system doesn't have one )
+    print(Z1) # Or get all information on the system by just printing it
+    print(Z2)
+system_test()
